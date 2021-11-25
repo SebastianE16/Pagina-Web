684fc93bcd5390a9f28efe0a064a72394c8d5e91
SebastianE16 <95038396+SebastianE16@users.noreply.github.com>
2021-11-25 10:44:47 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang= "es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preload" href="css/reseteo.css" as="styles">
+    <link rel="stylesheet" href="css/reseteo.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2? family=Krub:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="preload" href="css/estilon.css" as="styles">
+    <link rel="stylesheet" href="css/estilon.css">
+    <title>Refaccionerias Gerardo</title>
+</head>
+<body>
+
+    <header>
+        <h1>Refaccionerias Gerardo</h1>
+    </header>
+    <div class="nav-bg">
+        <nav class="nave-principal contenedor">
+                <a href="#">Inicio</a>
+                <a href="#">Nosotros</a>
+                <a href="#">Clientes</a>
+                <a href="#">Contactos</a>
+        </nav>
+    </div>
+    
+   <section class="hero">
+       <div class="contenido-hero">
+          <h2>Las mejores refacciones para su vehiculo automotriz</h2>
+          <div class="ubicacion">
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-map-pins" width="76" height="76" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M10.828 9.828a4 4 0 1 0 -5.656 0l2.828 2.829l2.828 -2.829z" />
+  <line x1="8" y1="7" x2="8" y2="7.01" />
+  <path d="M18.828 17.828a4 4 0 1 0 -5.656 0l2.828 2.829l2.828 -2.829z" />
+  <line x1="16" y1="15" x2="16" y2="15.01" />
+</svg>
+                   
+                <p>
+                    Veracruz, México
+                </p>
+          </div>
+       <div>
+         <a class="boton" href="#">Contactar</a>
+       </div>
+       
+   </section>
+   <main class="contenedor sombra">
+    <h2>Mis servicios</h2>
+        <div class="servicios">
+              <section class="servicio">
+                    <h3>Refacciones de todo tipo</h3>
+                    <div class="iconos">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-steam" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M16.5 5a4.5 4.5 0 1 1 -.653 8.953l-4.347 3.009l0 .038a3 3 0 0 1 -2.824 2.995l-.176 .005a3 3 0 0 1 -2.94 -2.402l-2.56 -1.098v-3.5l3.51 1.755a2.989 2.989 0 0 1 2.834 -.635l2.727 -3.818a4.5 4.5 0 0 1 4.429 -5.302z" />
+  <circle fill="currentColor" cx="16.5" cy="9.5" r="1" />
+</svg>
+                    </div>
+                    <p>
+                    Somos especialistas en refacciones y accesorios para la industria automotriz. Nuestro catálogo incluye amortiguadores, calaveras, espejos, defensas, manijas, tolvas, soportes de motor, soportes de transmisión, mazas, faros, clutch, bombas de gasolina, sensores cpk, sensores de oxígeno, cremalleras, elevadores de ventana, bomba de frenos radiadores, balatas, discos de freno, focos de xenón, focos y faros de LED. También contamos con accesorios para motocicletas.
+                    </p>
+            </section>
+            <section class="servicio">
+                <h3>Piezas para todas las necesidades</h3>
+                <div class="iconos">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-manual-gearbox" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <circle cx="5" cy="6" r="2" />
+  <circle cx="12" cy="6" r="2" />
+  <circle cx="19" cy="6" r="2" />
+  <circle cx="5" cy="18" r="2" />
+  <circle cx="12" cy="18" r="2" />
+  <line x1="5" y1="8" x2="5" y2="16" />
+  <line x1="12" y1="8" x2="12" y2="16" />
+  <path d="M19 8v2a2 2 0 0 1 -2 2h-12" />
+</svg>
+                </div>
+<p>
+Al comprar con nosotros tiene la tranquilidad de que los productos son originales, todos fabricados y respaldados por marcas como Brembo, Monroe, Moog, SYD, Dorman, Rancho, KYB, BOGE, Standard, entre otros.
+</p>
+            </section>
+            <section class="servicio">
+                <h3>Taller Automotriz</h3>
+                <div class="iconos">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-tool" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M7 10h3v-3l-3.5 -3.5a6 6 0 0 1 8 8l6 6a2 2 0 0 1 -3 3l-6 -6a6 6 0 0 1 -8 -8l3.5 3.5" />
+</svg>
+                </div>
+                <p>
+                Tambien contamos con un taller en nuestras instalaciones para cualquier tipo de modificacion o emergencia que necesite con personal capacitado para brindarle todo tipo de atencion a su vehiculo que pueda necesitar
+                </p>
+            </section> 
+            <section class="servicio">
+                <h3>Contamos tambien con grua</h3>
+                <div class="iconos">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-car-crane" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <circle cx="5" cy="17" r="2" />
+  <circle cx="17" cy="17" r="2" />
+  <path d="M7 18h8m4 0h2v-6a5 5 0 0 0 -5 -5h-1l1.5 5h4.5" />
+  <path d="M12 18v-11h3" />
+  <path d="M3 17v-5h9" />
+  <path d="M4 12v-6l18 -3v2" />
+  <path d="M8 12v-4l-4 -2" />
+</svg>
+                </div>
+                <p>
+                De llegar a necesitarla solo debe marcar a nuestro numero telefonico +52 922 282 7304 o mandar un mail a Refaccioneriasgerardo@gmail.com
+                </p>
+            </section>
+            <section class="servicio">
+                <h3>Encuentrenos en</h3>
+                <div class="iconos">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-map-pin" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <circle cx="12" cy="11" r="3" />
+  <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" />
+</svg>
+                </div>
+                <p>
+                Carretera Transístmica, 18 de Octubre, Cuarto, 96340 Cosoleacaque, Ver.
+                </p>
+            </section>
+        </div>
+
+    <section>
+        <h2>Contacto</h2>
+            <form class="formulario">
+            <fieldset>
+                <legend>Contáctanos llenando todos los campos</legend>
+                    <div class="contenedor-campos">
+                        <div class="campo">
+                            <label>Nombre</label>
+                            <input class="input-text" type="text" placeholder="Nombre">
+                        </div>
+                        <div class="campo">
+                            <label>Teléfono</label>
+                            <input class="input-text" type="tel" placeholder="Teléfono">
+                        
+                        </div>
+                        <div class="campo">
+                            <label>Correo</label>
+                            <input class="input-text" type="mail" placeholder="Correo">
+                        
+                        </div>
+                        <div class="campo">
+                            <label>Mensaje</label>
+                            <textarea class="input-text"></textarea>
+                        
+                        </div>
+                    </div>
+                    <div class="alinear-derecha flex">
+                       <input class="boton w-sm-100" type="button" value="Enviar">
+                    </div>
+                    
+            </fieldset>
+            </form>
+    </section>
+    </main>
+    <footer class="pie">
+        <p>Todos los derechos reservados. Don Gerardo Mendez</p>
+    </footer>
+</body>
+</html>
